f23caf1423b650e6cf958d2bdb142805cf3b2138
griddery <97827977+griddery@users.noreply.github.com>
2022-01-16 22:56:19 +0800

    Update grid.js

--- grid.js
+++ grid.js
@@ -15,20 +15,20 @@ window.griddery = function(TT_TT, boobahz){
 	$(".col-item").each(function(){
 		$(this).not(".col-item + .col-item").each(function(){
 			if(JQversion >= "180"){
-				$(this).nextUntil(":not(.col-item)").addBack().wrapAll('<div class="col-table">');
+				$(this).nextUntil(":not(.col-item)").addBack().wrapAll('<div class="col-tr">');
 			} else {
-				$(this).nextUntil(":not(.col-item)").andSelf().wrapAll('<div class="col-table">');
+				$(this).nextUntil(":not(.col-item)").andSelf().wrapAll('<div class="col-tr">');
 			}
 		})
 	})//end .col-item each
 	
 	for(smh=1; smh<col_count+1; smh++){
-		$(".col-table .col-item:nth-child(" + col_count + "n+" + smh + ")").each(function(){
+		$(".col-tr .col-item:nth-child(" + col_count + "n+" + smh + ")").each(function(){
 			$(this).attr("col-id",smh)
 		});
 	}
 	
-	$(".col-table").prepend("<div class='temp-cols'></div>");
+	$(".col-tr").prepend("<div class='temp-cols'></div>");
 	
 	for(tytn=1; tytn<col_count+1; tytn++){
         $(".temp-cols").append("<div class='col-column' col-id='" + tytn + "'></div>");
@@ -38,7 +38,7 @@ window.griddery = function(TT_TT, boobahz){
 	
 	$(".col-item").each(function(){
         var col_id = $(this).attr("col-id");
-        $(this).appendTo($(this).parents(".col-table").find(".col-column[col-id='" + col_id + "']"))
+        $(this).appendTo($(this).parents(".col-tr").find(".col-column[col-id='" + col_id + "']"))
     })
     
     $(".col-column[col-id], .col-item[col-id]").removeAttr("col-id");
